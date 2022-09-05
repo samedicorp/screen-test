3b4997e8721fa5fb2e0137cb7388a9c8c17c6e6e
Sam Deane <sam@elegantchaos.com>
2022-09-05 21:51:19 +0100

    added arrow buttons

--- main.lua
+++ main.lua
@@ -51,17 +51,47 @@ local bufferField
 fieldState = fieldState or {}
 local screen = toolkit.Screen.new()
 local layer = screen:addLayer()
-layer:addButton({100, 100, 60, 20}, "test", function()
+
+layer:addButton({50, 100, 60, 20}, "test", function()
     bufferField:append("test pressed", true)
 end)
-layer:addButton({100, 200, 60, 20}, "other", { 
-    style = "lineStyle", 
+
+layer:addButton({50, 130, 60, 20}, "other", { 
+    style = "line", 
     onMouseUp = function()
         bufferField:append("other pressed", true)
     end
 })
 
-startRect = startRect or {100, 300, 60, 20}
+layer:addButton({50, 200, 20, 20}, "up", { 
+    style = "leftArrow", 
+    onMouseUp = function()
+        bufferField:append("left pressed", true)
+    end
+})
+
+layer:addButton({80, 200, 20, 20}, "up", { 
+    style = "upArrow", 
+    onMouseUp = function()
+        bufferField:append("up pressed", true)
+    end
+})
+
+layer:addButton({110, 200, 20, 20}, "up", { 
+    style = "downArrow", 
+    onMouseUp = function()
+        bufferField:append("down pressed", true)
+    end
+})
+
+layer:addButton({140, 200, 20, 20}, "down", { 
+    style = "rightArrow", 
+    onMouseUp = function()
+        bufferField:append("right pressed", true)
+    end
+})
+
+startRect = startRect or {50, 300, 60, 20}
 layer:addButton(startRect, "dragme", {
     onMouseDrag = function(pos, button)
         if not buttonOffset then
